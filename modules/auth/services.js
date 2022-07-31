d168ddec7ff9e924229528bc39b340702521feca
Andy <andyarias@serempre.com>
2022-07-31 14:25:14 -0500

    service auth

---
+++ modules/auth/services.js
@@ -0,0 +1,28 @@
+const config = require('../../config/env')
+const jwt = require('jsonwebtoken');
+const userService = require('../users/services');
+const bcrypt = require('bcrypt');
+const boom = require('@hapi/boom');
+
+exports.siginToken = (user) => {
+    const payload = {
+        sub: user.id,
+    }
+    const token = jwt.sign(payload, config.jwtSecret, { expiresIn: config.jwtExpirationTime })
+    const data = { user, token }
+    return data;
+}
+
+exports.getUser = async (email, password) => {
+    const user = await userService.findByEmail(email);
+    if (!user) {
+        throw boom.unauthorized();
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw boom.unauthorized();
+    }
+    return user;
+}
