b87e413a3b435ed5620686ad9382aa0d5a4139ec
Andy <andyarias@serempre.com>
2022-07-31 15:01:04 -0500

    added cors

--- app/index.js
+++ app/index.js
@@ -7,10 +7,12 @@ const env = require('../config/env');
 const routes = require('./routes/api');
 const cronManager = require('./utils/cron-setup');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const cors = require('cors');
 //END APP LIBRARIES
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 require('./utils/auth')
 
 routes(app);
