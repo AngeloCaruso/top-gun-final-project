76337465e35afde7b811503771d7725a701bb36a
Andy <andyarias@serempre.com>
2022-08-11 12:01:29 -0500

    better message for confirm password

--- modules/users/schema.js
+++ modules/users/schema.js
@@ -3,6 +3,9 @@ const Joi = require('joi');
 exports.createUserSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-    repeat_password: Joi.ref('password'),
+    repeat_password: Joi.any().equal(Joi.ref('password'))
+    .required()
+    .label('Confirm password')
+    .options({ messages: { 'any.only': '{{#label}} does not match'} }),
 })
 
